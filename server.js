d6148ce79306294a6b53ed501e85793bce3822e6
Cyan-bug <cyanbug@tuta.io>
2025-04-28 18:40:07 +0200

    Update3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const runCrawler = require('./crawler/index');
+const runCrawler = require('./crawler/index.js');
 
 app.use(express.json());
 
